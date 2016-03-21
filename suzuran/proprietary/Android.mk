44557d994d432b8d4a7614d42ace285fe1be3e04
CTXz <ctx.xda@gmail.com>
2016-03-22 02:33:01 +0400

    suzuran: Deffer PRODUCT_COPY_FILES

--- suzuran/proprietary/Android.mk
+++ suzuran/proprietary/Android.mk
@@ -155,12 +155,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/firmware
 include $(BUILD_PREBUILT)
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     system/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
     system/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
     
 # Required by audio.primary.msm8994.so
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     lib/libsonydseehxwrapper.so:system/lib/libsonydseehxwrapper.so \
     lib/libaudioroute.so:system/lib/libaudioroute.so \
     lib64/libsonydseehxwrapper.so:system/lib64/libsonydseehxwrapper.so \
