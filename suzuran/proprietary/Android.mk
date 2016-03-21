018214be6ab027c8de70841e6ac537abb6cd8d6f
CTXz <ctx.xda@gmail.com>
2016-03-22 02:47:06 +0400

    suzuran: Move binary blobs product construct to suzuran-blobs.mk

--- suzuran/proprietary/Android.mk
+++ suzuran/proprietary/Android.mk
@@ -154,16 +154,4 @@ LOCAL_MODULE_SUFFIX := .bin
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/firmware
 include $(BUILD_PREBUILT)
-
-PRODUCT_COPY_FILES += \
-    system/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
-    system/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
-    
-# Required by audio.primary.msm8994.so
-PRODUCT_COPY_FILES += \
-    lib/libsonydseehxwrapper.so:system/lib/libsonydseehxwrapper.so \
-    lib/libaudioroute.so:system/lib/libaudioroute.so \
-    lib64/libsonydseehxwrapper.so:system/lib64/libsonydseehxwrapper.so \
-    lib64/libaudioroute.so:system/lib64/libaudioroute.so \
-
 endif
---
+++ suzuran/proprietary/suzuran-blobs.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += \
+    system/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
+    system/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
+    
+# Required by audio.primary.msm8994.so
+PRODUCT_COPY_FILES += \
+    lib/libsonydseehxwrapper.so:system/lib/libsonydseehxwrapper.so \
+    lib/libaudioroute.so:system/lib/libaudioroute.so \
+    lib64/libsonydseehxwrapper.so:system/lib64/libsonydseehxwrapper.so \
+    lib64/libaudioroute.so:system/lib64/libaudioroute.so \
