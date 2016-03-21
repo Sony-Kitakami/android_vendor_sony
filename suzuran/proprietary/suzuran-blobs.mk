ea5fe0403ad3ff949dd875e20d0e44b17159057d
CTXz <ctx.xda@gmail.com>
2016-03-22 03:23:33 +0400

    suzuran: Add correct path

--- suzuran/proprietary/suzuran-blobs.mk
+++ suzuran/proprietary/suzuran-blobs.mk
@@ -1,10 +1,12 @@
+LOCAL_PATH := vendor/sony/suzuran/proprietary
+
 PRODUCT_COPY_FILES += \
-    system/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
-    system/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
+    $(LOCAL_PATH)/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
+    $(LOCAL_PATH)/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
     
 # Required by audio.primary.msm8994.so
 PRODUCT_COPY_FILES += \
-    lib/libsonydseehxwrapper.so:system/lib/libsonydseehxwrapper.so \
-    lib/libaudioroute.so:system/lib/libaudioroute.so \
-    lib64/libsonydseehxwrapper.so:system/lib64/libsonydseehxwrapper.so \
-    lib64/libaudioroute.so:system/lib64/libaudioroute.so
+    $(LOCAL_PATH)/lib/libsonydseehxwrapper.so:system/lib/libsonydseehxwrapper.so \
+    $(LOCAL_PATH)/lib/libaudioroute.so:system/lib/libaudioroute.so \
+    $(LOCAL_PATH)/lib64/libsonydseehxwrapper.so:system/lib64/libsonydseehxwrapper.so \
+    $(LOCAL_PATH)/lib64/libaudioroute.so:system/lib64/libaudioroute.so
